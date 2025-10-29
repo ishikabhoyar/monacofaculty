758b5661c1f8300d620bfbfce0a25a74210c26c4
ishikabhoyar <ishikabhoyar2005@gmail.com>
2025-10-29 16:58:48 +0530

    feat: add edge runtime support to various API routes and pages

--- Frontend/app/tests/[id]/questions/page.tsx
+++ Frontend/app/tests/[id]/questions/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = 'edge';
+
 import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { getQuestionsForTest, createQuestion, updateQuestion, deleteQuestion, bulkCreateQuestions, getTestCasesForQuestion, createTestCase, updateTestCase, deleteTestCase } from '../../../../lib/facultyApi';
